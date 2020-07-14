e1ce08cdfa03e913dc9ab801b42743d32f1f87f6
liuyong <liuyong@soyoung.com>
2020-07-14 15:39:46 +0800

    push

--- controllers/Hello.go
+++ controllers/Hello.go
@@ -11,8 +11,7 @@ func HelloWorld(c *gin.Context) {
 	mobile := c.DefaultQuery("mobile", "")
 	maps := make(map[string]interface{})
 	maps["username"] = mobile
-	var adminModel models.Admin
-	result := adminModel.GetAdmin(maps)
+	result := models.GetAdmin(maps)
 	c.JSON(http.StatusOK, gin.H{
 		"code": 200,
 		"msg":  "请求成功",
--- models/Admin.go
+++ models/Admin.go
@@ -1,6 +1,6 @@
 package models
 
-func (ad *Admin) GetAdmin(maps interface{}) (admin Admin) {
+func GetAdmin(maps interface{}) (admin Admin) {
 	db.Select("username").Where(maps).First(&admin)
 	return
 }
