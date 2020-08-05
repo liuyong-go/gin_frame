627cd80f483f3d37e0c010bc8f4072aadd544d42
liuyong <liuyong@soyoung.com>
2020-08-05 14:41:19 +0800

    test

--- controllers/Hello.go
+++ controllers/Hello.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"gin_frame/models"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,7 +12,7 @@ func HelloWorld(c *gin.Context) {
 	maps := make(map[string]interface{})
 	maps["username"] = mobile
 	result := models.GetAdmin(maps)
-	time.Sleep(10 * time.Second)
+	// time.Sleep(10 * time.Second)
 	c.JSON(http.StatusOK, gin.H{
 		"code": 200,
 		"msg":  "请求成功1",
