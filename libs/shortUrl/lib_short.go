cf11385cdc9ee4346fcfee970b3d323f9789919b
liuyong <liuyong@soyoung.com>
2020-08-22 18:01:14 +0800

    shorturl

---
+++ libs/shortUrl/lib_short.go
@@ -0,0 +1,17 @@
+package libs
+
+//初始化短链配置
+func init() {
+
+}
+
+//CreateShortURL 生成短链接
+func CreateShortURL(lurl string) (surl string) {
+
+	return
+}
+
+//GetURL 根据短链接获取长链接
+func GetURL(surl string) (lurl string) {
+	return
+}
